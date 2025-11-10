416b799e993b1528c3b45bd40f7b43496977df1f
diffth <diffth@nate.com>
2025-11-10 19:14:20 +0900

    GameFPS_CPP

--- Intermediate/Build/Win64/UnrealEditor/Inc/GameFPS_CPP/UHT/GameFPS_CPP.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/GameFPS_CPP/UHT/GameFPS_CPP.init.gen.cpp
@@ -7,23 +7,27 @@
 #include "UObject/GeneratedCppIncludes.h"
 PRAGMA_DISABLE_DEPRECATION_WARNINGS
 void EmptyLinkFunctionForGeneratedCodeGameFPS_CPP_init() {}
+	GAMEFPS_CPP_API UFunction* Z_Construct_USparseDelegateFunction_GameFPS_CPP_ShooterGameModeOnEndGame__DelegateSignature();
 	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_GameFPS_CPP;
 	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_GameFPS_CPP()
 	{
 		if (!Z_Registration_Info_UPackage__Script_GameFPS_CPP.OuterSingleton)
 		{
+			static UObject* (*const SingletonFuncArray[])() = {
+				(UObject* (*)())Z_Construct_USparseDelegateFunction_GameFPS_CPP_ShooterGameModeOnEndGame__DelegateSignature,
+			};
 			static const UECodeGen_Private::FPackageParams PackageParams = {
 				"/Script/GameFPS_CPP",
-				nullptr,
-				0,
+				SingletonFuncArray,
+				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x8E7942FC,
-				0x2898C241,
+				0x6A1F1EFC,
+				0xFB590A8E,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_GameFPS_CPP.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_GameFPS_CPP.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_GameFPS_CPP(Z_Construct_UPackage__Script_GameFPS_CPP, TEXT("/Script/GameFPS_CPP"), Z_Registration_Info_UPackage__Script_GameFPS_CPP, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x8E7942FC, 0x2898C241));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_GameFPS_CPP(Z_Construct_UPackage__Script_GameFPS_CPP, TEXT("/Script/GameFPS_CPP"), Z_Registration_Info_UPackage__Script_GameFPS_CPP, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x6A1F1EFC, 0xFB590A8E));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
