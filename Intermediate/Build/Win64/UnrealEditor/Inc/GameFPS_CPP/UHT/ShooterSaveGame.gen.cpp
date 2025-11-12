18870da47fca7e7042fb786c81a0740d509a51ef
diffth <diffth@nate.com>
2025-11-12 21:55:56 +0900

    ShooterSaveGame

--- Intermediate/Build/Win64/UnrealEditor/Inc/GameFPS_CPP/UHT/ShooterSaveGame.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/GameFPS_CPP/UHT/ShooterSaveGame.gen.cpp
@@ -28,6 +28,17 @@ void EmptyLinkFunctionForGeneratedCodeShooterSaveGame() {}
 #if WITH_METADATA
 		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
 #endif
+		static const UECodeGen_Private::FFloatPropertyParams NewProp_ScoresCPP_Inner;
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_ScoresCPP_MetaData[];
+#endif
+		static const UECodeGen_Private::FArrayPropertyParams NewProp_ScoresCPP;
+		static const UECodeGen_Private::FStrPropertyParams NewProp_TimesCPP_Inner;
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam NewProp_TimesCPP_MetaData[];
+#endif
+		static const UECodeGen_Private::FArrayPropertyParams NewProp_TimesCPP;
+		static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
 		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
 		static const UECodeGen_Private::FClassParams ClassParams;
 	};
@@ -45,6 +56,28 @@ void EmptyLinkFunctionForGeneratedCodeShooterSaveGame() {}
 		{ "ModuleRelativePath", "Private/Gameplay/ShooterSaveGame.h" },
 	};
 #endif
+	const UECodeGen_Private::FFloatPropertyParams Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_ScoresCPP_Inner = { "ScoresCPP", nullptr, (EPropertyFlags)0x0000000000020000, UECodeGen_Private::EPropertyGenFlags::Float, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, 0, METADATA_PARAMS(0, nullptr) };
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_ScoresCPP_MetaData[] = {
+		{ "Category", "ShooterSaveGame" },
+		{ "ModuleRelativePath", "Private/Gameplay/ShooterSaveGame.h" },
+	};
+#endif
+	const UECodeGen_Private::FArrayPropertyParams Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_ScoresCPP = { "ScoresCPP", nullptr, (EPropertyFlags)0x0010000000020005, UECodeGen_Private::EPropertyGenFlags::Array, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(UShooterSaveGame, ScoresCPP), EArrayPropertyFlags::None, METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_ScoresCPP_MetaData), Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_ScoresCPP_MetaData) };
+	const UECodeGen_Private::FStrPropertyParams Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_TimesCPP_Inner = { "TimesCPP", nullptr, (EPropertyFlags)0x0000000000020000, UECodeGen_Private::EPropertyGenFlags::Str, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, 0, METADATA_PARAMS(0, nullptr) };
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_TimesCPP_MetaData[] = {
+		{ "Category", "ShooterSaveGame" },
+		{ "ModuleRelativePath", "Private/Gameplay/ShooterSaveGame.h" },
+	};
+#endif
+	const UECodeGen_Private::FArrayPropertyParams Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_TimesCPP = { "TimesCPP", nullptr, (EPropertyFlags)0x0010000000020005, UECodeGen_Private::EPropertyGenFlags::Array, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(UShooterSaveGame, TimesCPP), EArrayPropertyFlags::None, METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_TimesCPP_MetaData), Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_TimesCPP_MetaData) };
+	const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_UShooterSaveGame_Statics::PropPointers[] = {
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_ScoresCPP_Inner,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_ScoresCPP,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_TimesCPP_Inner,
+		(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UShooterSaveGame_Statics::NewProp_TimesCPP,
+	};
 	const FCppClassTypeInfoStatic Z_Construct_UClass_UShooterSaveGame_Statics::StaticCppClassTypeInfo = {
 		TCppClassTypeTraits<UShooterSaveGame>::IsAbstract,
 	};
@@ -54,15 +87,16 @@ void EmptyLinkFunctionForGeneratedCodeShooterSaveGame() {}
 		&StaticCppClassTypeInfo,
 		DependentSingletons,
 		nullptr,
-		nullptr,
+		Z_Construct_UClass_UShooterSaveGame_Statics::PropPointers,
 		nullptr,
 		UE_ARRAY_COUNT(DependentSingletons),
 		0,
-		0,
+		UE_ARRAY_COUNT(Z_Construct_UClass_UShooterSaveGame_Statics::PropPointers),
 		0,
 		0x000000A0u,
 		METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_UShooterSaveGame_Statics::Class_MetaDataParams), Z_Construct_UClass_UShooterSaveGame_Statics::Class_MetaDataParams)
 	};
+	static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_UShooterSaveGame_Statics::PropPointers) < 2048);
 	UClass* Z_Construct_UClass_UShooterSaveGame()
 	{
 		if (!Z_Registration_Info_UClass_UShooterSaveGame.OuterSingleton)
@@ -83,9 +117,9 @@ void EmptyLinkFunctionForGeneratedCodeShooterSaveGame() {}
 		static const FClassRegisterCompiledInInfo ClassInfo[];
 	};
 	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GameFPS_CPP_Source_GameFPS_CPP_Private_Gameplay_ShooterSaveGame_h_Statics::ClassInfo[] = {
-		{ Z_Construct_UClass_UShooterSaveGame, UShooterSaveGame::StaticClass, TEXT("UShooterSaveGame"), &Z_Registration_Info_UClass_UShooterSaveGame, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UShooterSaveGame), 3178607538U) },
+		{ Z_Construct_UClass_UShooterSaveGame, UShooterSaveGame::StaticClass, TEXT("UShooterSaveGame"), &Z_Registration_Info_UClass_UShooterSaveGame, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UShooterSaveGame), 2990275863U) },
 	};
-	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GameFPS_CPP_Source_GameFPS_CPP_Private_Gameplay_ShooterSaveGame_h_2789035670(TEXT("/Script/GameFPS_CPP"),
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GameFPS_CPP_Source_GameFPS_CPP_Private_Gameplay_ShooterSaveGame_h_3066867729(TEXT("/Script/GameFPS_CPP"),
 		Z_CompiledInDeferFile_FID_GameFPS_CPP_Source_GameFPS_CPP_Private_Gameplay_ShooterSaveGame_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_GameFPS_CPP_Source_GameFPS_CPP_Private_Gameplay_ShooterSaveGame_h_Statics::ClassInfo),
 		nullptr, 0,
 		nullptr, 0);
